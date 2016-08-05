c82c83abc6381da5345244ff0181dd3cbb417a3a
rubak <rubak@math.aau.dk>
2016-08-05 22:19:37 +0200

    Tweak for Windows compiling.

--- src/geometry/base/port.h
+++ src/geometry/base/port.h
@@ -102,7 +102,7 @@ typedef uint16_t u_int16_t;
 
 // The following guarenty declaration of the byte swap functions, and
 // define __BYTE_ORDER for MSVC
-#ifdef COMPILER_MSVC
+#if defined COMPILER_MSVC || defined WIN32
 #include <stdlib.h>
 #define __BYTE_ORDER __LITTLE_ENDIAN
 #define bswap_16(x) _byteswap_ushort(x)
