6c0e91f0f0f86538d1c0e9bc2a6d2dcf9601477a
Ege Rubak <rubak@Julies-MacBook-Air-2.local>
2016-08-10 01:11:27 +0200

    Switch to Rcpp interface.

--- src/interval_test.cpp
+++ src/interval_test.cpp
@@ -1,16 +1,11 @@
 //// Code to test basic R interface ////
 
-#include <R.h>
+#include <Rcpp.h>
 #include "s2/s1interval.h"
 
-extern "C" {
-void interval_test_c(double* x, double* a, double* b, int* res) {
-  S1Interval my_int(*a, *b);
-  bool inside = my_int.Contains(*x);
-  if(inside) {
-    *res = 1;
-  } else {
-    *res = 0;
-  }
-}
+//[[Rcpp::export]]
+bool interval_test(double x, double a, double b) {
+  S1Interval my_int(a, b);
+  bool inside = my_int.Contains(x);
+  return inside;
 }
