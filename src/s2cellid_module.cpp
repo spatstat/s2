b774f906829bee6af807936ee61ed5c0ea47baee
rubak <rubak@math.aau.dk>
2017-02-09 14:51:23 +0800

    More CellId methods.

--- src/s2cellid_module.cpp
+++ src/s2cellid_module.cpp
@@ -7,12 +7,27 @@
 // S2CellId S2CellIdFromLatLng(S2LatLng const &ll){
 //   return S2CellId::FromLatLng(ll);
 // }
+// 
+// //[[Rcpp::export]]
+// std::string cellparent(double lat, double lng, int level){
+//   S2LatLng x = S2LatLng::FromDegrees(lat, lng);
+//   S2CellId id = S2CellId::FromLatLng(x);
+//   return id.parent(level).ToString();
+// }
+
+S2CellId S2CellId_parent(S2CellId* c, int level){
+  if(c->level() < level) Rcpp::stop("Supplied level larger than current level!");
+  return c->parent(level);
+}
 
 RCPP_MODULE(S2CellId_module){
   using namespace Rcpp;
   
   class_<S2CellId>("S2CellId")
   .constructor()
+  .method("face", &S2CellId::face)
+  .method("level", &S2CellId::level)
+  .method("parent", &S2CellId_parent)
   .method("ToPoint", &S2CellId::ToPoint)
   .method("ToString", &S2CellId::ToString)
   .method("ToToken", &S2CellId::ToToken)
