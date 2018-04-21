8f6fa4b38d9727ba575ea9070e8e80515ab66b40
Ege Rubak <rubak@math.aau.dk>
2018-04-21 22:18:56 +0200

    Add a quick example of point containment in cap.

--- R/s2cap.R
+++ R/s2cap.R
@@ -6,6 +6,10 @@
 #' @param cap Named list containing axis and height of cap.
 #' @param x Object(s) to test for containment in `cap`. At the moment only
 #' points are handled and they must be specified as a three-column matrix.
+#' @examples
+#' cap <- list(axis = c(0,1,0), height = 0.1)
+#' S2Cap_Contains(cap, matrix(c(0,1,0),ncol=3))
+#' S2Cap_Contains(cap, matrix(c(1,0,0),ncol=3))
 #' @export
 S2Cap_Contains <- function(cap, x){
   S2Cap_contains_point(x, cap)
